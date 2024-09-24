72e3d3a2df204fb9980ce9b7f1c46f4eb5c930fd
Shubham <shubhamkumarhms13@gmail.com>
2024-09-24 23:44:48 +0530

    First Commit

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,20 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "journal_entries")
+public class JournalEntry {
+    @Id
+    private ObjectId id;
+    @NonNull
+    private String title;
+    private String content;
+    private LocalDateTime date;
+}
