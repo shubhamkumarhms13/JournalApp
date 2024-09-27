5be3d023dde3a2a10e340aed07025f9b987c7252
Shubham <shubhamkumarhms13@gmail.com>
2024-09-27 18:23:05 +0530

    Integrated MongoDB

--- src/main/java/net/engineeringdigest/journalApp/repository/JournalEntryRepository.java
+++ src/main/java/net/engineeringdigest/journalApp/repository/JournalEntryRepository.java
@@ -4,6 +4,6 @@ import net.engineeringdigest.journalApp.entity.JournalEntry;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId>{
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
 }
--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -1,45 +1,34 @@
 package net.engineeringdigest.journalApp.service;
 
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-@Slf4j
 @Component
 public class JournalEntryService {
 
-    @Autowired
+    @Autowired      //we are injecting Journal Entry Repository in Journal Entry Service
     private JournalEntryRepository journalEntryRepository;
 
-//    public void saveEntry(JournalEntry journalEntry){
-//        try{
-//            journalEntry.setDate(LocalDateTime.now());
-//            journalEntryRepository.save(journalEntry);
-//        } catch (Exception e) {
-//            log.error("Exception ", e);
-//        }
-//    }
+
     public void saveEntry(JournalEntry journalEntry) {
         journalEntryRepository.save(journalEntry);
     }
 
-    public List<JournalEntry> getAll(){
+    public List<JournalEntry> getAll() {
         return journalEntryRepository.findAll();
     }
 
-    public Optional<JournalEntry> findById(ObjectId id){
+    public Optional<JournalEntry> findById(ObjectId id) {
         return journalEntryRepository.findById(id);
     }
 
-    public void deleteById(ObjectId id){
+    public void deleteById(ObjectId id) {
         journalEntryRepository.deleteById(id);
     }
 }
