fe1f5de5fe5b4774414d044e7632f1377671c0a0
Shubham <shubhamkumarhms13@gmail.com>
2024-11-04 22:50:04 +0530

    Spring Security (Modification Needed 3)

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -23,7 +23,6 @@ public class SpringSecurity {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         return http.authorizeHttpRequests(request -> request
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/journal/**", "/user/**").authenticated()
--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -6,9 +6,12 @@ import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +21,15 @@ public class UserService {
     @Autowired      //we are injecting UserRepository in UserService
     private UserRepository userRepository;
 
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void saveEntry(User user) {
+        userRepository.save(user);
+    }
+
+    public void saveNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
         userRepository.save(user);
     }
 
